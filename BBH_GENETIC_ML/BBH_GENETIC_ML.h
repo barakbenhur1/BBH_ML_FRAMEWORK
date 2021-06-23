49a64de49b6f0fce73053e6c7d6d7d2e569ea2f6
barak ben hur <barakbenhur@gmail.com>
2021-06-23 22:26:27 +0300

    Initial Commit

---
+++ BBH_GENETIC_ML/BBH_GENETIC_ML.h
@@ -0,0 +1,18 @@
+//
+//  BBH_GENETIC_ML.h
+//  BBH_GENETIC_ML
+//
+//  Created by Interactech on 23/06/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BBH_GENETIC_ML.
+FOUNDATION_EXPORT double BBH_GENETIC_MLVersionNumber;
+
+//! Project version string for BBH_GENETIC_ML.
+FOUNDATION_EXPORT const unsigned char BBH_GENETIC_MLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBH_GENETIC_ML/PublicHeader.h>
+
+
